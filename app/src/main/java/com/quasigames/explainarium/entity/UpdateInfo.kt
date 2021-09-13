ea34a1c975b45866e7f470b893cdcef3c37fee7e
Mikhail <mishantrop@users.noreply.github.com>
2021-09-13 15:44:22 +0300

    Проверка обновлений и обработка ошибок
    * Проверка обновлений и обработка ошибок
    * Игнорирование релизного билда

--- app/src/main/java/com/quasigames/explainarium/entity/UpdateInfo.kt
+++ app/src/main/java/com/quasigames/explainarium/entity/UpdateInfo.kt
@@ -1,5 +1,9 @@
 package com.quasigames.explainarium.entity
 
+import com.quasigames.explainarium.BuildConfig
+
 data class UpdateInfo(
     val VERSION_CODE: Int,
-)
+) {
+    constructor() : this(BuildConfig.VERSION_CODE)
+}
